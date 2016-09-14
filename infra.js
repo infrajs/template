561cc1b30874924611402d9f49a45bcb5f68bd9e
Anton Kiyatkin <axlbant@sent.com>
2016-09-14 06:36:48 +0400

    Update infra.js

--- infra.js
+++ infra.js
@@ -1,12 +1,11 @@
 
-infra.template.scope.infra.config=function(name){
-	var conf=infra.config();
-	if(!name) return conf;
-	return conf[name];
+Template.scope.infra.config=function(name){
+	return Config.get(name);
 }
-infra.template.scope.Config = {};
-infra.template.scope.Config.get=function(name){
-	var conf=infra.config();
-	if(!name) return conf;
-	return conf[name];
+Event.one('Controller.oninit', function () {
+	Template.scope['~conf'] = Config.get();
+});
+Template.scope.Config = {};
+Template.scope.Config.get=function(name){
+	return Config.get(name);
 }
