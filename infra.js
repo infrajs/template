100f35f146fe619e173ad0a4a0fa46a48cbe53cd
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-12 22:58:24 +0400

    fix

--- infra.js
+++ infra.js
@@ -1,38 +1,5 @@
-import { Config } from '/vendor/infrajs/config/Config.js'	
-import { Template } from '/vendor/infrajs/template/Template.js'
-import { Load } from '/vendor/infrajs/load/Load.js'
-import { Seq } from '/vendor/infrajs/sequence/Seq.js'
-import { Access } from '/vendor/infrajs/access/Access.js'
-import { Path } from '/vendor/infrajs/path/Path.js'
-import { View } from '/vendor/infrajs/view/View.js'
+import { Controller } from '/vendor/infrajs/controller/src/Controller.js'
 
-Template.scope['~data'] = function (src) {
-	return Load.loadJSON(src);
-}
-
-
-Template.scope.infra.config=function(name){
-	return Config.get(name);
-}
-
-Template.scope['~conf'] = Config.get();//deprecated
-
-Template.scope.Config = {};
-Template.scope.Config.get=function(name){
-	return Config.get(name);
-}
-Seq.set(Template.scope, ['View', 'getHost'], function () { return View.getHost();} );
-
-
-Template.scope['Path'] = {};
-Template.scope['Path']['encode'] = function (str) {
-	return Path.encode(str);
-}
-
-Template.scope['Access'] = {};
-Template.scope['Access']['adminTime'] = function () {
-	return Access.adminTime();
-};
-Template.scope['Access']['getDebugTime'] = function () {
-	return Access.getDebugTime();
-};
+Controller.hand('init', async () => {
+	await import('./init.js')
+})
---
+++ init.js
@@ -0,0 +1,34 @@
+import { Config } from '/vendor/infrajs/config/Config.js'	
+import { Template } from '/vendor/infrajs/template/Template.js'
+import { Load } from '/vendor/infrajs/load/Load.js'
+import { Seq } from '/vendor/infrajs/sequence/Seq.js'
+import { Access } from '/vendor/infrajs/access/Access.js'
+import { Path } from '/vendor/infrajs/path/Path.js'
+import { View } from '/vendor/infrajs/view/View.js'
+
+Template.scope['~data'] = function (src) {
+	return Load.loadJSON(src);
+}
+
+
+Template.scope['~conf'] = Config.get();
+
+Template.scope.Config = {};
+Template.scope.Config.get=function(name){
+	return Config.get(name);
+}
+Seq.set(Template.scope, ['View', 'getHost'], function () { return View.getHost();} );
+
+
+Template.scope['Path'] = {};
+Template.scope['Path']['encode'] = function (str) {
+	return Path.encode(str);
+}
+
+Template.scope['Access'] = {};
+Template.scope['Access']['adminTime'] = function () {
+	return Access.adminTime();
+};
+Template.scope['Access']['getDebugTime'] = function () {
+	return Access.getDebugTime();
+};
